5785e460e137c41613863d6062179bbeda9e524b
Michal Sedlak <sedlakmichal@gmail.com>
2013-02-02 18:45:09 +0100

    Add login notice and improved menu links

--- lib/Treex/Web/Controller/Scenario.pm
+++ lib/Treex/Web/Controller/Scenario.pm
@@ -37,17 +37,17 @@ sub base :Chained('/') :PathPart('') :CaptureArgs(0)  {
         ),
         template => 'scenario.tt2',
     );
-    
+
     $c->stash( user_scenarios => $c->user->search_related_rs('scenarios') )
         if $c->user_exists;
 }
 
 sub object :Chained('base') :PathPart('scenario') :CaptureArgs(1) {
     my ( $self, $c, $scenario_id ) = @_;
-    
+
     my $scenario = $c->model('WebDB::Scenario')->find($scenario_id);
     $c->stash(scenario => $scenario);
-    
+
     $c->detach($self->action_for('not_found'))
         unless $scenario;
 }
@@ -65,28 +65,28 @@ sub my_scenarios :Chained('base') :PathPart('my/scenarios') :Args(0) {
     }
 
     $c->stash(template => 'scenario/my.tt2');
-}    
+}
 
 sub not_found :Private {
     my ( $self, $c ) = @_;
-    
+
     $c->response->status(404);
     $c->stash('template' => 'scenario/not_found.tt2');
 }
-    
+
 sub add :Chained('base') :PathPart('scenario/add') :Args(0) {
     my ( $self, $c ) = @_;
     my $form = $c->stash->{'scenarioForm'};
-    
+
     if ( $c->req->method eq 'POST' && $c->user_exists ) {
         my $new_scenario = $c->model('WebDB::Scenario')->new_result({ user => $c->user->id });
-        
+
         if ($form->process(item => $new_scenario, params => $c->req->parameters)) {
             $c->flash->{status_msg} = 'Scenario successfully created';
             $c->response->redirect($c->uri_for($self->action_for('index')));
         }
     }
-    
+
     $c->stash(template => 'scenario/new.tt2');
 }
 
@@ -97,7 +97,7 @@ sub delete :Chained('object') :PathPart('delete') :Args(0) {
 
     $c->detach('not_found')
         unless ( $c->user_exists && $scenario->user->id eq $c->user->id);
-    
+
     if ($scenario->delete) {
         $c->flash->{status_msg} = 'Scenario successfully deleted';
     } else {
