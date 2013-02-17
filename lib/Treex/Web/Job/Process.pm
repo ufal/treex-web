56f8fb8cb79633b91de59ab3311a9b3986d88480
Michal Sedlak <sedlakmichal@gmail.com>
2013-02-17 20:31:03 +0100

    Set timeout back to 30 seconds

--- lib/Treex/Web/Job/Process.pm
+++ lib/Treex/Web/Job/Process.pm
@@ -38,7 +38,7 @@ sub perform {
     push @cmd, "Write::Treex to=result.treex";
 
     open my $err, ">error.log" or die $!;
-    my $timeout = 5;
+    my $timeout = 30;
     my ( $ret, $h );
     try {
         $h = harness \@cmd, '<', \undef, '>&', $err, (my $t = timeout($timeout, exception => Treex::Web::Job::Exception::TimedOut->new()));
