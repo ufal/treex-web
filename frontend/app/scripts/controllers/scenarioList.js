b04f2dcf15036295b70557e8b38c4a8a980292e4
Michal Sedlak <sedlakmichal@gmail.com>
2013-07-10 04:34:22 +0200

    Improving listing #79

--- frontend/app/scripts/controllers/scenarioList.js
+++ frontend/app/scripts/controllers/scenarioList.js
@@ -4,29 +4,43 @@ angular.module('TreexWebApp').controller(
   'ScenarioListCtrl',
   ['$scope', '$filter', 'Scenario', 'Treex',
    function($scope, $filter, Scenario, Treex) {
+     $scope.languages = Scenario.languages();
      $scope.languagesMap = Treex.languagesMap();
      $scope.status = 'loading';
-     $scope.update = function(params) {
-       Scenario.query({ language : '' }, function(scenarios) {
-         if (scenarios.length == 0) {
-           $scope.status = 'empty';
-           $scope.totalFound = 0;
-           return [];
-         }
-         var maxPages = Math.ceil(scenarios.length / params.count),
-             pager = {};
 
-         $scope.status = 'results';
-         $scope.pager = pager = {
-           current : params.page > maxPages ? maxPages : params.page,
-           count : maxPages,
-           countPerPage : params.count
-         };
-         if (params.sorting && params.sorting[0]) {
-           scenarios = $filter('orderBy')(scenarios, params.sorting[0], params.sortingDirection[0]);
-         }
-         $scope.scenarios = scenarios.slice((pager.current-1)*pager.countPerPage, pager.current*pager.countPerPage);
-       });
+     $scope.$watch('language', function(value) {
+       if (lastParams) {
+         lastParams.page = 1;
+         lastParams.filter || (lastParams.filter = {});
+         lastParams.filter['languages'] = value;
+         $scope.update(lastParams);
+       }
+     });
+
+     var scenarios = [],
+         lastParams;
+     $scope.update = function(params) {
+       var maxPages = Math.ceil(scenarios.length / params.count),
+           pager = {},
+           source = scenarios;
+
+       lastParams = params;
+       $scope.status = 'results';
+       $scope.pager = pager = {
+         current : params.page > maxPages ? maxPages : params.page,
+         count : maxPages,
+         countPerPage : params.count
+       };
+       if (params.filter && params.filter.languages) {
+         var lang = parseInt(params.filter.languages);
+         source = $filter('filter')(source, function(value) {
+           return value.languages.indexOf(lang) != -1;
+         });
+       }
+       if (params.sorting && params.sorting[0]) {
+         source = $filter('orderBy')(source, params.sorting[0], params.sortingDirection[0]);
+       }
+       $scope.scenarios = source.slice((pager.current-1)*pager.countPerPage, pager.current*pager.countPerPage);
      };
 
      $scope.pager = {
@@ -35,8 +49,16 @@ angular.module('TreexWebApp').controller(
        countPerPage: 10
      };
 
-     $scope.update({
-       page: 1,
-       count: 10
+     Scenario.query({ language : '' }, function(data) {
+       if (data.length == 0) {
+         $scope.status = 'empty';
+         $scope.totalFound = 0;
+         return [];
+       }
+       scenarios = data;
+       $scope.update({
+         page: 1,
+         count: 10
+       });
      });
    }]);
