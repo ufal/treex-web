13bd46a28e5b134fb367e3099a798ec8ded228e8
Michal Sedlak <sedlakmichal@gmail.com>
2013-07-24 04:11:56 +0200

    Fix missing input when loaded from url

--- frontend/app/scripts/controllers/inputUrl.js
+++ frontend/app/scripts/controllers/inputUrl.js
@@ -11,6 +11,7 @@ angular.module('TreexWebApp')
         $timeout(function() {
           $scope.$modal('layout');
         });
+        $scope.text = data;
         return data;
       }, function(reason) {
         $scope.loading = false;
