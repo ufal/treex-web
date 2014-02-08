630f6de37318c555f4f9de050ce83feb8cb05d9b
Michal Sedlák <sedlakmichal@gmail.com>
2014-02-08 13:26:08 +0100

    Add GA tracking

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('TreexWebApp', ['$strap.directives', 'http-auth-interceptor', 'blueimp.fileupload', 'ngResource', 'ngRoute'])
+angular.module('TreexWebApp', ['$strap.directives', 'http-auth-interceptor', 'blueimp.fileupload', 'ngResource', 'ngRoute', 'angulartics', 'angulartics.google.analytics'])
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/', { templateUrl: 'views/home.html', controller: 'MainCtrl' }).
